f7ea5e3f6ff70afb39be4e51f9412802eb7063f9
Zhiqiang.Wong <wongzq.dev@gmail.com>
2022-03-04 17:28:02 +0800

    # 系统设置菜单；

--- DW_DEVICES_OCC/DW_DEVICES_OCC/OCC_Components/OCC/Forms/OCC_MAIN.Designer.cs
+++ DW_DEVICES_OCC/DW_DEVICES_OCC/OCC_Components/OCC/Forms/OCC_MAIN.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.MainFormContent = new System.Windows.Forms.Panel();
+            this.SettingDropDownMenu = new Sunny.UI.UIContextMenuStrip();
+            this.系统设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SettingDropDownMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainFormContent
@@ -43,12 +46,29 @@
             this.MainFormContent.Size = new System.Drawing.Size(1274, 727);
             this.MainFormContent.TabIndex = 0;
             // 
+            // SettingDropDownMenu
+            // 
+            this.SettingDropDownMenu.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.SettingDropDownMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.系统设置ToolStripMenuItem});
+            this.SettingDropDownMenu.Name = "SettingDropDownMenu";
+            this.SettingDropDownMenu.Size = new System.Drawing.Size(145, 30);
+            // 
+            // 系统设置ToolStripMenuItem
+            // 
+            this.系统设置ToolStripMenuItem.Name = "系统设置ToolStripMenuItem";
+            this.系统设置ToolStripMenuItem.Size = new System.Drawing.Size(144, 26);
+            this.系统设置ToolStripMenuItem.Text = "系统设置";
+            // 
             // OCC
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1280, 800);
             this.Controls.Add(this.MainFormContent);
             this.ExtendBox = true;
+            this.ExtendMenu = this.SettingDropDownMenu;
+            this.ExtendSymbol = 61459;
+            this.ExtendSymbolOffset = new System.Drawing.Point(0, 1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(1280, 800);
@@ -58,6 +78,7 @@
             this.ShowRadius = false;
             this.Text = "OCC";
             this.Load += new System.EventHandler(this.OCC_MAIN_Load);
+            this.SettingDropDownMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -65,5 +86,7 @@
         #endregion
 
         private System.Windows.Forms.Panel MainFormContent;
+        private Sunny.UI.UIContextMenuStrip SettingDropDownMenu;
+        private System.Windows.Forms.ToolStripMenuItem 系统设置ToolStripMenuItem;
     }
 }
